00debee262432481482d5f672fddae7dfdbe7bf8
kavvadias <kavvadias@pop-os.localdomain>
2023-01-05 17:17:16 +0100

    did a 180 boi, locking nft validator

--- src/Minting.hs
+++ src/Minting.hs
@@ -15,141 +15,125 @@
 
 module Minting where
 
-import           Cardano.Api                          (PlutusScript,
-                                                       PlutusScriptV2,
-                                                       writeFileTextEnvelope)
-import           Cardano.Api.Shelley                  (PlutusScript (..),
-                                                       ScriptDataJsonSchema (ScriptDataJsonDetailedSchema),
-                                                       fromPlutusData,
-                                                       scriptDataToJson)
+import           Cardano.Api                                           (PlutusScript,
+                                                                        PlutusScriptV2,
+                                                                        writeFileTextEnvelope)
+import           Cardano.Api.Shelley                                   (PlutusScript (..),
+                                                                        ScriptDataJsonSchema (ScriptDataJsonDetailedSchema),
+                                                                        fromPlutusData,
+                                                                        scriptDataToJson)
 import           Codec.Serialise
-import           Data.Aeson                           as A
-import           Data.ByteString.Lazy                 as LBS
-import qualified Data.ByteString.Lazy                 as LBS
-import qualified Data.ByteString.Short                as SBS
-import           Data.Functor                         (void)
-import           GHC.Generics                         (Generic)
+import           Data.Aeson                                            as A
+import qualified Data.ByteString.Lazy                                  as LBS
+import qualified Data.ByteString.Short                                 as SBS
+import           Data.Functor                                          (void)
+import           GHC.Generics                                          (Generic)
 
 import           Plutus.Contract
 
 import           Ledger
-import           Ledger.Constraints                   as Constraints
-import qualified Ledger.Typed.Scripts                 as Scripts
-import           Ledger.Value                         as Value
+import           Ledger.Constraints                                    as Constraints
+import           Ledger.Value                                          as Value
 
-import qualified Plutus.Script.Utils.V2.Scripts       as PSU.V2
-import qualified Plutus.Script.Utils.V2.Typed.Scripts as Utils.V2
-import qualified Plutus.V2.Ledger.Api                 as PlutusV2
-import           Plutus.V2.Ledger.Contexts            (ownCurrencySymbol)
+import qualified Plutus.Script.Utils.V2.Scripts                        as Scripts
+import qualified Plutus.Script.Utils.V2.Typed.Scripts                  as TScripts
+import qualified Plutus.Script.Utils.V2.Typed.Scripts.MonetaryPolicies as Scripts
+import           Plutus.V1.Ledger.Value                                as V
+import qualified Plutus.V2.Ledger.Api                                  as Api
+import           Plutus.V2.Ledger.Contexts                             as Api
 
 import           Plutus.Trace
-import qualified Plutus.Trace.Emulator                as Emulator
-import qualified Wallet.Emulator.Wallet               as Wallet
+import qualified Plutus.Trace.Emulator                                 as Emulator
+import qualified Wallet.Emulator.Wallet                                as Wallet
 
 import qualified PlutusTx
-import           PlutusTx.Prelude                     as Plutus hiding
-                                                                (Semigroup (..),
-                                                                 unless, (.))
+import           PlutusTx.Prelude                                      as Plutus hiding
+                                                                                 (Semigroup (..),
+                                                                                  unless,
+                                                                                  (.))
 
-import           Control.Monad.Freer.Extras           as Extras
-import           Data.Text                            (Text)
-import           Prelude                              (FilePath, IO,
-                                                       Semigroup (..),
-                                                       Show (..), String, print,
-                                                       (.))
+import           Control.Monad.Freer.Extras                            as Extras
+import           Data.Text                                             (Text)
+import           Prelude                                               (FilePath,
+                                                                        IO,
+                                                                        Semigroup (..),
+                                                                        Show (..),
+                                                                        String,
+                                                                        print,
+                                                                        (.))
 
 
-data PropParams = PropParams { ppIndex     :: BuiltinByteString
-                             --, ppPrev      :: PlutusV2.TxOutRef
-                             , ppState     :: BuiltinByteString
-                             , ppQuestions :: BuiltinByteString
-                             , ppAnswers   :: [BuiltinByteString]
-                             , ppResults   :: [(Integer, BuiltinByteString)]
-                             } deriving (Generic,ToJSON, FromJSON, Show)
+data DatumProp = DatumProp { pName      :: !BuiltinByteString
+                           --, ppPrev      :: Api.TxOutRef
+                           , pState     :: !BuiltinByteString
+                           , pQuestions :: !BuiltinByteString
+                           , pAnswers   :: ![BuiltinByteString]
+                           , pResults   :: ![(Integer, BuiltinByteString)]
+                           } deriving (Generic,ToJSON, FromJSON, Show)
 
-PlutusTx.makeLift ''PropParams
-PlutusTx.unstableMakeIsData ''PropParams
+PlutusTx.makeLift ''DatumProp
+PlutusTx.makeIsDataIndexed ''DatumProp [('DatumProp,0)]
 
-{-# INLINABLE exColateralPolicy #-}
-exColateralPolicy :: () -> PropParams -> PlutusV2.ScriptContext -> Bool
-exColateralPolicy _ pp ctx = traceIfFalse "Wrong Redeemer!" $ checkRedeemer pp
+{-# INLINABLE propUpdateVal #-}
+propUpdateVal :: DatumProp -> () -> Api.ScriptContext -> Bool
+propUpdateVal dp _ ctx = traceIfFalse "no mint/burn wallet signiature" checkSign &&
+                         traceIfFalse "no nft pair" checkNfts
 
   where
-    checkRedeemer :: PropParams -> Bool
-    checkRedeemer (PropParams _ s _ a r) = s ==  "496e6974" && a == [] && r == []
+    txInfo :: Api.TxInfo
+    txInfo = Api.scriptContextTxInfo ctx
+
+    txInValues :: [(CurrencySymbol, TokenName, Integer)]
+    txInValues = concat $ flattenValue `map` (Api.txOutValue `map` (Api.txInInfoResolved `map` Api.txInfoInputs txInfo))
+
+    txMint :: [(CurrencySymbol, TokenName, Integer)]
+    txMint = flattenValue (Api.txInfoMint txInfo)
+
+    valEq :: (CurrencySymbol, TokenName, Integer) -> (CurrencySymbol, TokenName, Integer) -> Bool
+    valEq = \(cs,tkn, _) (cs', tkn', _) -> cs == cs' && unTokenName tkn == (unTokenName tkn' <> "_A")
+
+    checkSign:: Bool
+    checkSign = "5867c3b8e27840f556ac268b781578b14c5661fc63ee720dbeab663f" `elem` map getPubKeyHash (Api.txInfoSignatories txInfo)
+
+    checkNfts :: Bool
+    checkNfts =  True --flattenValue txMint flattenValue
 
 data Typed
-instance Utils.V2.ValidatorTypes Typed where
-  type instance DatumType Typed = ()
-  type instance RedeemerType Typed = PropParams
+instance TScripts.ValidatorTypes Typed where
+  type instance DatumType Typed = DatumProp
+  type instance RedeemerType Typed = ()
 
-tvalidator :: Utils.V2.TypedValidator Typed
-tvalidator = Utils.V2.mkTypedValidator @Typed
-    $$(PlutusTx.compile [|| exColateralPolicy ||])
+tvalidator :: TScripts.TypedValidator Typed
+tvalidator = TScripts.mkTypedValidator @Typed
+    $$(PlutusTx.compile [|| propUpdateVal ||])
     $$(PlutusTx.compile [|| wrap ||])
       where
-          wrap = Utils.V2.mkUntypedValidator @() @PropParams
+          wrap = TScripts.mkUntypedValidator @DatumProp @()
 
-policy :: Utils.V2.MintingPolicy
-policy = Utils.V2.forwardingMintingPolicy tvalidator
+validator :: Scripts.Validator
+validator = TScripts.validatorScript tvalidator
 
-policyHash :: PSU.V2.MintingPolicyHash
-policyHash = PSU.V2.mintingPolicyHash policy
+valHash :: Scripts.ValidatorHash
+valHash = TScripts.validatorHash tvalidator
 
-curSymbol :: CurrencySymbol
-curSymbol = PSU.V2.scriptCurrencySymbol policy
+scrAddr :: Address
+scrAddr = TScripts.validatorAddress tvalidator
 
--- Off-Chain
+-- Write Stuff
 
-type MintSchema =
-    Endpoint "mintin" PropParams
-
-prop :: BuiltinByteString
-prop = "proposal"
-
-mintin :: AsContractError e => PropParams -> Contract w s e ()
-mintin (PropParams i s q a r) =  do
-                            let redeemer = PlutusV2.toBuiltinData
-                                         $ PropParams { ppIndex     = i
-                                                      , ppState     = s
-                                                      , ppQuestions = q
-                                                      , ppAnswers   = a
-                                                      , ppResults   = r }
-                                tknName = tokenName $ fromBuiltin $ prop <> i
-                                tx = Constraints.mustMintCurrencyWithRedeemer policyHash (Redeemer redeemer) tknName 1
-                            ledgerTx <- submitTxConstraints tvalidator tx
-                            void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-
-endpoints :: Contract () MintSchema Text ()
-endpoints = awaitPromise mintin' >> endpoints
-  where
-    mintin' = endpoint @"mintin" mintin
-
---Emulator
-
-test :: IO ()
-test = runEmulatorTraceIO $ do
-  h1 <- activateContractWallet (Wallet.knownWallet 1) endpoints
-  h2 <- activateContractWallet (Wallet.knownWallet 2) endpoints
-  callEndpoint @"mintin" h1 $ redeemerTest
-  void $ Emulator.waitNSlots 10
-  s <- Emulator.waitNSlots 10
-  Extras.logInfo $ "End of Simulation at slot " ++ show s
---Write Stuff
-
-redeemerTest :: PropParams
-redeemerTest = PropParams { ppIndex = "0001",
-                            ppState = "Init",
-                            ppQuestions = "Does it work?",
-                            ppAnswers = [],
-                            ppResults = []
+redeemerTest :: DatumProp
+redeemerTest = DatumProp { pName= "0001",
+                           pState = "Init",
+                           pQuestions = "Does it work?",
+                           pAnswers = [],
+                           pResults = []
                           }
 
 printRedeemer :: IO ()
-printRedeemer = print $ "Redeemer: " <> A.encode (scriptDataToJson ScriptDataJsonDetailedSchema $ fromPlutusData $ PlutusV2.toData redeemerTest)
+printRedeemer = print $ "Redeemer: " <> A.encode (scriptDataToJson ScriptDataJsonDetailedSchema $ fromPlutusData $ Api.toData redeemerTest)
 
-script :: PlutusV2.Script
-script = PlutusV2.unMintingPolicyScript policy
+script :: Api.Script
+script = Api.unValidatorScript validator
 
 scriptSBS :: SBS.ShortByteString
 scriptSBS = SBS.toShort . LBS.toStrict $ serialise script
@@ -161,7 +145,7 @@ writeSerialisedScript :: IO ()
 writeSerialisedScript = void $ writeFileTextEnvelope "testnet/nftMint.plutus" Nothing serialisedScript
 
 writeJSON :: PlutusTx.ToData a => FilePath -> a -> IO ()
-writeJSON file = LBS.writeFile file . A.encode . scriptDataToJson ScriptDataJsonDetailedSchema . fromPlutusData . PlutusV2.toData
+writeJSON file = LBS.writeFile file . A.encode . scriptDataToJson ScriptDataJsonDetailedSchema . fromPlutusData . Api.toData
 
 writeRedeemer :: IO ()
 writeRedeemer = writeJSON "testnet/redeemer.json" ()
