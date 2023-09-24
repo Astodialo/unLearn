65fc5616266027fe6c34ed0e8017109d4a835da6
Astodialo <panteliskavv@gmail.com>
2023-09-24 12:51:11 +0200

    meshy

--- offChain/pages/api/create-mining-transaction.ts
+++ offChain/pages/api/create-mining-transaction.ts
@@ -2,11 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import {
   AppWallet,
   ForgeScript,
+  PlutusScript,
   Transaction,
   KoiosProvider,
   largestFirst,
   keepRelevant,
-  readPlutusData
+  readPlutusData,
+  resolvePlutusScriptAddress
 } from "@meshsdk/core";
 import { resolveDataHash } from '@meshsdk/core';
 import type { Mint, Data, Unit, Quantity } from "@meshsdk/core";
@@ -16,6 +18,7 @@ import {
   idanikoMetadata,
   costLovelace,
 } from "../../config/mint";
+import { prop_mint_code }  from "../../../lucid/params"
 
 
 export default async function handler(
@@ -40,7 +43,6 @@ export default async function handler(
   }
 
   const unArxhUnit = buildUnit(policy, 'unArxh');
-  const idanikoUnit = buildUnit(policy, 'idaniko')
 
   const koios = new KoiosProvider('preprod');
 
@@ -55,48 +57,37 @@ export default async function handler(
   });
 
   const appWalletAddress = appWallet.getPaymentAddress();
-  const forgingScript = ForgeScript.withOneSignature(appWalletAddress);
+
+  const mintingScript: PlutusScript = {
+    code: prop_mint_code,
+    version: 'V2',
+  };
+  const minting_addr = resolvePlutusScriptAddress(mintingScript)
 
 
   /**
    * TODO: Here you want to select one of your NFT that has not been minted
    */
 
-  const unArxhUTXO = await koios.fetchAddressUTxOs( 'unArxi validator addr' )
+  const [unArxhUTXO] = await koios.fetchAddressUTxOs(minting_addr, unArxhUnit)
 
-  const assetId = "0"
-  if (unArxhUTXO.length !== 0) {
-      const scriptUtxos = await koios.fetchAddressUTxOs('unArxh validator addr', 'unArxi assetId')
-      const utxo = scriptUtxos[0];
-      const datumMetadataCBOR = utxo.output.plutusData;
-      const datumMetadata = readPlutusData(datumMetadataCBOR);
-      const [ assetId ] = datumMetadata.fields
-  }
+  const datumMetadataCBOR = unArxhUTXO.output.plutusData;
+  const datum = readPlutusData(datumMetadataCBOR);
+  const [ name, count ] = datum.fields
+  
   
   // unArxh NFTs
   
   // --Datum--
   
   // The datum of the uArxh nft that is going to be locked in the unArxh Validator. In the datum of this NFT are located the general data of the dao, like the number of proposals (TxId used for the minting of the proposal NFTs).The mint of it and the idaniko NFT is the genesis of the dao. It is going to be updated in each proposal NFT minting, in a way acting as a couner.
-  const unArxh_assetId: Data = new Map<Data, Data>();
-  unArxh_assetId.set('Id', assetId+1)
 
   const unArxhMetadata: Data = {
     alternative: 0,
-    fields: [unArxh_assetId]
+    fields: [name, count + 1]
   };
 
   // The datum of an nft that is going to be locked in the unArxh validator. After the first mint where the unArxh validator address is empty, this NFT is going to stay forever locked, ensuring not to be moved on the Plutus validator. The reason is so that there can never be a point after the first mint that someone can mint the unArxh and idaniko NFTs. It will contain in the metadata what the ideals of the dao are.
-
-  const idaniko_ena: Data = new Map<Data, Data>();
-  idaniko_ena.set('unLearn', 'Forum')
-
-  const idanikoDatum: Data = {
-    alternative: 0,
-    fields: [idaniko_ena]
-  }
-
-  // The minting of the unArxh genesis Nft
 
   const unArxh: Mint = {
     assetName: "unArxh",
@@ -104,58 +95,34 @@ export default async function handler(
     metadata: {name: "unArxh"},
     label: "721",
     recipient: {
-      address: recipientAddress,
+      address: minting_addr,
       datum: {value: unArxhMetadata, inline : true}
     },
   };
 
-  // The miniting of the idaniko Nft
-
-  const idaniko: Mint = {
-    assetName: "idaniko",
-    assetQuantity: "1",
-    metadata: {idanikoMetadata},
-    label: "721",
-    recipient: {
-      address: recipientAddress,
-      datum: {value: idanikoDatum, inline : true}
-    },
-  };
-
-  
   // Proposal NFT 
   
   const assetIdPrefix = "proposal-"
-  const assetName = `${assetIdPrefix}${assetId}`;
+  const assetName = `${assetIdPrefix}${count}`;
 
   //  --Datum--
-  const datumN: Data = new Map<Data, Data>();
-  datumN.set('name', assetName );
+  const datumN: Data = assetName;
 
-  const datumT: Data = new Map<Data, Data>();
-  datumT.set('type', '') // It can either be a simple proposal or proposal for funding etc. 
+  const datumP: Data = input;
 
-  const datumQ: Data = new Map<Data, Data>();
-  datumQ.set('question', input);
-
-  const datumA: Data = new Map<Data, Data>();
-  datumA.set('answers', []);
-
-  const datumR: Data = new Map<Data, Data>();
-  datumR.set('results', []);
+  const datumR: Data = "";
   
-  const datumState: Data = new Map<Data, Data>();
-  datumState.set('state', 'INIT');
+  const datumState: Data = 'INIT';
 
-  const datumAmount: Data = new Map<Data, Data>();
-  datumAmount.set('amount', 0);
-
+  const datumAmount: Data = 0;
 
   const datumMetadata: Data = {
     alternative: 0,
-    fields: [datumN, datumT, datumQ, datumA, datumR, datumState, datumAmount]
+    fields: [datumN, datumP , datumR, datumState, datumAmount]
   };
-
+  const redeemer = { 
+    data: { alternative: 0, fields: []},
+  };
   // Minting for reference NFT to be sent and locked in the validator with the datum containing the data of the progress. At this stage the fields Name, Type, Question, State are filled. The other are null.  
   const assetQ: Mint = {
     assetName: assetName,
@@ -169,10 +136,20 @@ export default async function handler(
   };
 
   // Minting for user Answers NFT to be sent to the user/proposer to be letter burned in a multisig tx to update the answers field in the refence NFT's datum. 
-  const assetA: Mint = {
-    assetName: assetName + "_A",
+  const assetR: Mint = {
+    assetName: assetName + "_R",
     assetQuantity: "1",
-    metadata: {name: assetName + "_A"},
+    metadata: {name: assetName + "_R"},
+    label: "721",
+    recipient: {
+      address: recipientAddress,
+    },
+  };
+
+  const assetClaim: Mint = {
+    assetName: assetName + "_Claim",
+    assetQuantity: "1",
+    metadata: {name: assetName + "_Claim"},
     label: "721",
     recipient: {
       address: recipientAddress,
@@ -180,7 +157,8 @@ export default async function handler(
   };
 
   const proposalUnit = buildUnit(policy, assetName);
-  const answersUnit = buildUnit(policy, assetName + "_A");
+  const resultsUnit = buildUnit(policy, assetName + "_R");
+  const claimUnit = buildUnit(policy, assetName + "_Claim");
 
   const outputsMap: Map<Unit, Quantity> = new Map<Unit, Quantity>();
   outputsMap.set('lovelace', costLovelace+5000000 );
@@ -188,11 +166,19 @@ export default async function handler(
   const selectedUtxos = keepRelevant(outputsMap, utxos, '15000000',);
 
   const tx = new Transaction({ initiator: appWallet });
-  tx.setTxInputs(selectedUtxos);
-  tx.mintAsset(forgingScript, assetQ,);
-  tx.mintAsset(forgingScript, assetA,);
-  tx.mintAsset(forgingScript, unArxh,);
-  tx.mintAsset(forgingScript, idaniko,);
+  tx.setTxInputs(selectedUtxos.concat(unArxhUTXO));
+  tx.mintAsset(mintingScript, assetQ, redeemer);
+  tx.mintAsset(mintingScript, assetR, redeemer);
+  tx.mintAsset(mintingScript, assetClaim, redeemer);
+  tx.sendAssets(
+    { address: minting_addr,
+      datum: {
+        value: unArxhMetadata,
+        inline: true,
+      },
+    },
+    [{unit: unArxhUnit, quantity: "1",},]
+  )
   tx.sendLovelace(recipientAddress, costLovelace);
   tx.setChangeAddress(recipientAddress);
 
